8e640c170b502c7babbca63430c5cdda25d5d448
KienHa05 <kienhgph50150@gmail.com>
2025-02-06 19:23:07 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index",{
-        pageTitle: "Trang Danh Sách Sản Phẩm",
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price * (1 - (item.discountPercentage / 100))).toFixed(0);
+        return item;
+    });
+
+    res.render("client/pages/products/index", {
+        pageTitle: "Danh Sách Sản Phẩm",
+        products: newProducts,
     });
 }
