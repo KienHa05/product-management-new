3c227d709d7eb11f2af01922994d306ed939c26a
KienHa05 <kienhgph50150@gmail.com>
2025-02-07 16:50:07 +0700

    3. Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,12 +3,48 @@ const Product = require("../../models/product.model");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
 
-    const products = await Product.find({
-        deleted: false
-    });
+    let filterStatus = [
+        {
+            name: "Tất Cả",
+            status: "",
+            class: "",
+            buttonColor: "primary"
+        },
+        {
+            name: "Hoạt Động",
+            status: "active",
+            class: "",
+            buttonColor: "success"
+        },
+        {
+            name: "Dừng Hoạt Động",
+            status: "inactive",
+            class: "",
+            buttonColor: "danger"
+        }
+    ];
     
+    if (req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active";
+    } else {
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+    }
+
+    let find = {
+        deleted: false
+    };
+
+    if (req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
+
     res.render("admin/pages/products/index", {
         pageTitle: "Danh Sách Sản Phẩm",
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 }
