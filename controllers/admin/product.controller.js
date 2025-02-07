55ceba424aa0f8c002ababba103e44ced813294e
KienHa05 <kienhgph50150@gmail.com>
2025-02-07 14:02:03 +0700

    2. Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,14 @@
+const Product = require("../../models/product.model");
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        deleted: false
+    });
+    
     res.render("admin/pages/products/index", {
-        pageTitle: "Danh Sách Sản Phẩm"
+        pageTitle: "Danh Sách Sản Phẩm",
+        products: products
     });
 }
