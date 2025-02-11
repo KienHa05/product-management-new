104155621cac27e4ba326ab8a9d287ed2a2e9597
KienHa05 <kienhgph50150@gmail.com>
2025-02-11 09:28:02 +0700

    4. Tính Năng Tạo Mới Sản Phẩm ( Bổ sung ảnh Preview )
    Buối số 25,  Very Noisy :((((

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -144,7 +144,6 @@ if (formChangeMulti) {
 
 // Show Alert
 const showAlert = document.querySelector("[show-alert]");
-
 if (showAlert) {
     const time = parseInt(showAlert.getAttribute("data-time"));
     const closeAlert = document.querySelector("[close-alert]");
@@ -158,3 +157,18 @@ if (showAlert) {
     });
 }
 // End Show Alert
+
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+    const uploadImageInput = document.querySelector("[upload-image-input]");
+    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+}
+// End Upload Image
