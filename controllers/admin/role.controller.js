5ad1b522eb837667a35a8c843137c32995bc8a27
KienHa05 <kienhgph50150@gmail.com>
2025-02-13 22:56:38 +0700

    4. Xây Dựng Phần Phân Quyền
    Buổi 31

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -63,6 +63,33 @@ module.exports.editPatch = async (req, res) => {
     } catch (error) {
         req.flash("error", "Cập Nhật Nhóm Quyền Thất Bại!");
     }
-    
+
     res.redirect(`back`);
+};
+
+// [GET] /admin/roles/permissions
+module.exports.permissions = async (req, res) => {
+    let find = {
+        deleted: false
+    };
+
+    const records = await Role.find(find);
+
+    res.render("admin/pages/roles/permissions", {
+        pageTitle: "Phân Quyền",
+        records: records,
+    });
+};
+
+// [PATCH] /admin/roles/permissions
+module.exports.permissionsPatch = async (req, res) => {
+    const permissions = JSON.parse(req.body.permissions);
+
+
+    for (const item of permissions) {
+        await Role.updateOne({ _id: item.id }, { permissions: item.permissions });
+    }
+
+    req.flash("success", "Cập Nhật Phân Quyền Thành Công!");
+    res.redirect("back");
 };
