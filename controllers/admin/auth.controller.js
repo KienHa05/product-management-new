9b9769fc794a2cb1740e193c3cf64c29d1524fd9
KienHa05 <kienhgph50150@gmail.com>
2025-02-14 16:47:50 +0700

    2. Làm Tính Năng Đăng Nhập
    Buổi 32

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,43 @@
+const md5 = require('md5');
+const Account = require("../../models/account.model");
+
+const systemConfig = require("../../config/system");
+
+// [GET] /admin/auth/login
+module.exports.login = (req, res) => {
+    res.render("admin/pages/auth/login", {
+        pageTitle: "Đăng Nhập"
+    });
+}
+
+// [POST] /admin/auth/login
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await Account.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if (!user) {
+        req.flash("error", "Email Không Tồn Tại!");
+        res.redirect("back");
+        return;
+    }
+
+    if (md5(password) != user.password) {
+        req.flash("error", "Mật Khẩu Không Chính Xác. Vui Lòng Kiểm Tra Lại");
+        res.redirect("back");
+        return;
+    }
+
+    if (user.status == "inactive") {
+        req.flash("error", "Tài Khoản Này Đang Bị Khóa!");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("token", user.token);
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+}
---
+++ validates/admin/auth.validate.js
@@ -0,0 +1,16 @@
+module.exports.loginPost = (req, res, next) => {
+
+    if (!req.body.email) {
+        req.flash("error", "Vui Lòng Nhập Email!");
+        res.redirect("back");
+        return;
+    }
+
+    if (!req.body.password) {
+        req.flash("error", "Vui Lòng Nhập Mật Khẩu!");
+        res.redirect("back");
+        return;
+    }
+
+    next();
+}
