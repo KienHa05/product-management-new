c8effc5d119bd9653fcbb4535314b18685fe87db
KienHa05 <kienhgph50150@gmail.com>
2025-02-15 15:26:13 +0700

    2. Hiển Thị Menu Chung Bên Client
    BUỔI 34
    
    Mở rộng thêm các tính năng mới như :
    + Tin Tức
    + Blog => Bài viết,....

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 
 module.exports = (app) => {
+    app.use(categoryMiddleware.category); // Tối ưu dạng middleware bên Client khi mở rộng
+
     app.use("/", homeRoutes);
 
     app.use("/products", productRoutes);
