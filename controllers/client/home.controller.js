c2b7ac2b5fca24c4d8eb6c12374fd7a23063c429
KienHa05 <kienhgph50150@gmail.com>
2025-02-15 21:24:39 +0700

    4.1 Hiển Thị Danh Sách Sản Phẩm Nổi Bật
    Buổi 35

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,21 @@
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helpers/products");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+    // Lấy Ra Sản Phẩm Nổi Bật
+    const productsFeatured = await Product.find({
+        featured: "1",
+        deleted: false,
+        status: "active",
+    }).limit(6);
+
+    const newProducts = productsHelper.priceNewProducts(productsFeatured);
+    // End Ra Sản Phẩm Nổi Bật
+
     res.render("client/pages/home/index", {
-        pageTitle: "Trang Chủ"
+        pageTitle: "Trang Chủ",
+        productsFeatured: newProducts,
     });
 }
