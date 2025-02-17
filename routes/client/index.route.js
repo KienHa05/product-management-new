fdf0864ee4132277f9dd15433d97cd7a520aefa5
KienHa05 <kienhgph50150@gmail.com>
2025-02-17 11:36:51 +0700

    2.1 Thêm Sản Phẩm Vào Giỏ Hàng
    BUỔI 36

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,15 +1,21 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 const searchRoutes = require("./search.route");
+const cartRoutes = require("./cart.route");
+
 
 module.exports = (app) => {
     app.use(categoryMiddleware.category); // Tối ưu dạng middleware bên Client khi mở rộng
+    app.use(cartMiddleware.cartId);
 
     app.use("/", homeRoutes);
 
     app.use("/products", productRoutes);
 
     app.use("/search", searchRoutes);
+
+    app.use("/cart", cartRoutes);
 }
