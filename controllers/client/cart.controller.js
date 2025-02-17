bb776cdf8a74e3eaf618bc4021f54e688cf52177
KienHa05 <kienhgph50150@gmail.com>
2025-02-17 15:01:30 +0700

    2.4 Xóa sản phẩm trong giỏ hàng ( Client )
    BUỔI 37

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -80,3 +80,22 @@ module.exports.addPost = async (req, res) => {
 
     res.redirect("back");
 }
+
+// [GET] /cart/delete/:productId
+module.exports.delete = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const productId = req.params.productId;
+
+    await Cart.updateOne(
+        {
+            _id: cartId,
+        },
+        {
+            $pull: { products: { product_id: productId } }
+        }
+    );
+
+    req.flash("success", "Đã Xóa Sản Phẩm Khỏi Giỏ Hàng!");
+
+    res.redirect("back");
+}
