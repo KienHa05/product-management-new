5d90c1eb746b284ef6b01f05d1d0e356c071f95a
KienHa05 <kienhgph50150@gmail.com>
2025-02-19 00:28:57 +0700

    1.2 Typing ( Phần 1 )
    BUỔI 41

--- public/js/chat.js
+++ public/js/chat.js
@@ -73,8 +73,21 @@ if (emojiPicker) {
     emojiPicker.addEventListener("emoji-click", (event) => {
         const icon = event.detail.unicode;
         inputChat.value += icon;
-    })
+    });
+
+    // Input Keyup
+    inputChat.addEventListener("keyup", () => {
+        socket.emit("CLIENT_SEND_TYPING", "show");
+    });
+    // End Input Keyup
 }
 // End Insert Icon To Input
 
 // End Show Icon Chat
+
+// SERVER_RETURN_TYPING
+socket.on("SERVER_RETURN_TYPING", (data) => {
+    console.log(data);
+});
+// End SERVER_RETURN_TYPING
+
