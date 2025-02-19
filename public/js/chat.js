ae72750dd1f41769a9abd64bc4a035ec5b2ad7db
KienHa05 <kienhgph50150@gmail.com>
2025-02-19 14:47:09 +0700

    1.4 Hiển Thị Ảnh Full Màn Hình
    BUỔI 42
    Sử dụng thêm thư viện ViewerJS để làm được chức năng ảnh full màn hình

--- public/js/chat.js
+++ public/js/chat.js
@@ -72,6 +72,9 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     body.insertBefore(div, boxTyping);
 
     body.scrollTop = body.scrollHeight;
+
+    // Preview Images
+    const gallery = new Viewer(div);
 });
 // End SERVER_RETURN_MESSAGE
 
@@ -173,4 +176,12 @@ if (elementListTyping) {
     });
 }
 // End SERVER_RETURN_TYPING
+
+// Preview Full Image
+const bodyChatPreviewImage = document.querySelector(".chat .inner-body");
+
+if (bodyChatPreviewImage) {
+    const gallery = new Viewer(bodyChatPreviewImage);
+}
+// End Preview Full Image
 
