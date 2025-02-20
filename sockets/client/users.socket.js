331090c69ffa5fcf34d1b2bec451c0ca7d21a7d2
KienHa05 <kienhgph50150@gmail.com>
2025-02-20 10:29:36 +0700

    1.4 A hủy kết bạn sẽ xóa A khỏi danh sách lời mời kết bạn của B
    BUỔI 44

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -111,6 +111,11 @@ module.exports = (res) => {
                 lengthAcceptFriends: lengthAcceptFriends
             });
 
+            // Lấy id của A và trả về cho B
+            socket.broadcast.emit("SERVER_RETURN_USER_ID_CANCEL_FRIEND", {
+                userIdB: userId,
+                userIdA: myUserId,
+            });
         });
 
         // Chức năng từ chối kết bạn
