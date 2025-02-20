f7f4105fe16859c4069ba4e926ffb507790ac6b1
KienHa05 <kienhgph50150@gmail.com>
2025-02-20 16:01:18 +0700

    2.2. Hiển thị realtime
    BUỔI 45
    Khi Login / Logout nó sẽ realtime luôn.

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -97,6 +97,13 @@ module.exports.loginPost = async (req, res) => {
         statusOnline: "online"
     });
 
+    _io.once('connection', (socket) => {
+        socket.broadcast.emit("SERVER_RETURN_USER_STATUS_ONLINE", {
+            userId: user.id,
+            status: "online"
+        });
+    });
+
     res.redirect("/");
 };
 
@@ -107,6 +114,13 @@ module.exports.logout = async (req, res) => {
         tokenUser: req.cookies.tokenUser
     }, {
         statusOnline: "offline"
+    });
+
+    _io.once("connection", (socket) => {
+        socket.broadcast.emit("SERVER_RETURN_USER_STATUS_ONLINE", {
+            userId: res.locals.user.id,
+            status: "offline"
+        });
     });
 
     res.clearCookie("tokenUser");
