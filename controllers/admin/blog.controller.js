b329d34d4cdbc2cd0d24cb4d4a21305e00570656
KienHa05 <kienhgph50150@gmail.com>
2025-05-27 16:08:51 +0700

    ( Blog ) Tính năng bộ lọc theo trạng thái bên Admin
    Gần như logic như cũ mà chỉ thay đổi khoảng 10-15% thôi.

--- controllers/admin/blog.controller.js
+++ controllers/admin/blog.controller.js
@@ -2,12 +2,48 @@ const Blog = require("../../models/blog.model");
 
 // [GET] /admin/blogs
 module.exports.index = async (req, res) => {
-  const blogs = await Blog.find({
+  let filterStatus = [
+    {
+      name: "Tất Cả",
+      status: "",
+      class: "",
+      buttonColor: "primary"
+    },
+    {
+      name: "Công Khai",
+      status: "published",
+      class: "",
+      buttonColor: "success"
+    },
+    {
+      name: "Bản Nháp",
+      status: "draft",
+      class: "",
+      buttonColor: "warning"
+    }
+  ];
+
+  if (req.query.status) {
+    const index = filterStatus.findIndex(item => item.status == req.query.status);
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex(item => item.status == "");
+    filterStatus[index].class = "active";
+  }
+
+  let find = {
     deleted: false
-  });
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const blogs = await Blog.find(find);
 
   res.render("admin/pages/blogs/index", {
     pageTitle: "Danh Sách Blog",
-    blogs: blogs
+    blogs: blogs,
+    filterStatus: filterStatus
   });
 };
