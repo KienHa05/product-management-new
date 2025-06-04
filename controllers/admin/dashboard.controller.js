90a6e6a477d17ca17db03ee8f8cedfbf2718db4f
KienHa05 <kienhgph50150@gmail.com>
2025-06-04 22:00:14 +0700

    Thêm thông tin liên quan tới Blog ở trang TỔNG QUAN ( dashboard admin )
    Nếu chuyên nghiệp hơn nữa thì có thể dành thời gian làm thêm biểu đồ Charts bên tổng quan như MUI,Ant Design,... để làm fullstack.

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,7 @@
 const ProductCategory = require("../../models/product-category.model");
 const Product = require("../../models/product.model");
+const BlogCategory = require("../../models/blog-category.model");
+const Blog = require("../../models/blog.model");
 const Account = require("../../models/account.model");
 const User = require("../../models/user.model");
 
@@ -17,6 +19,16 @@ module.exports.dashboard = async (req, res) => {
       active: 0,
       inactive: 0
     },
+    categoryBlog: {
+      total: 0,
+      active: 0,
+      inactive: 0
+    },
+    blog: {
+      total: 0,
+      published: 0,
+      draft: 0
+    },
     account: {
       total: 0,
       active: 0,
@@ -29,6 +41,7 @@ module.exports.dashboard = async (req, res) => {
     },
   };
 
+  // Danh Mục Sản Phẩm
   statistic.categoryProduct.total = await ProductCategory.count({
     deleted: false
   });
@@ -42,7 +55,9 @@ module.exports.dashboard = async (req, res) => {
     status: "inactive",
     deleted: false
   });
+  // End Danh Mục Sản Phẩm
 
+  // Sản Phẩm
   statistic.product.total = await Product.count({
     deleted: false
   });
@@ -56,7 +71,41 @@ module.exports.dashboard = async (req, res) => {
     status: "inactive",
     deleted: false
   });
+  // End Sản Phẩm
 
+  // Danh Mục Blog
+  statistic.categoryBlog.total = await BlogCategory.count({
+    deleted: false
+  });
+
+  statistic.categoryBlog.active = await BlogCategory.count({
+    status: "active",
+    deleted: false
+  });
+
+  statistic.categoryBlog.inactive = await BlogCategory.count({
+    status: "inactive",
+    deleted: false
+  });
+  // End Danh Mục Blog
+
+  // Blog
+  statistic.blog.total = await Blog.count({
+    deleted: false
+  });
+
+  statistic.blog.published = await Blog.count({
+    status: "published",
+    deleted: false
+  });
+
+  statistic.blog.draft = await Blog.count({
+    status: "draft",
+    deleted: false
+  });
+  // End Blog
+
+  // Tài Khoản Bên Admin
   statistic.account.total = await Account.count({
     deleted: false
   });
@@ -70,7 +119,9 @@ module.exports.dashboard = async (req, res) => {
     status: "inactive",
     deleted: false
   });
+  // End Tài Khoản Bên Admin
 
+  // Tài Khoản Bên Client
   statistic.user.total = await User.count({
     deleted: false
   });
@@ -84,6 +135,7 @@ module.exports.dashboard = async (req, res) => {
     status: "inactive",
     deleted: false
   });
+  // End Tài Khoản Bên Client
 
   res.render("admin/pages/dashboard/index", {
     pageTitle: "Trang Tổng Quan",
