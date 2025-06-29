7d9a74d445a09ca3d7ed9c8574780058088e72a0
KienHa05 <kienhgph50150@gmail.com>
2025-06-29 15:37:58 +0700

    Đổi utils hashPassword sang tools
    + utils mang nghĩa tái sử dụng tại nhiều nơi như bên helper (helper dùng nhiều nơi nhưng tính năng riêng biệt còn utils thì dùng dạng full hết toàn bộ)
    + tools mang nghĩa chạy IIFE 1 lần thôi thì sẽ đúng về nghĩa trong tổ chức dự án hơn.

--- tools/hashPassword.tool.js
+++ tools/hashPassword.tool.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcrypt');
 
-// Có thể copy nguyên hàm IIFE này vứt sang bên giao diện [GET] Login rồi chạy là OK
+// Có thể nhúng require nguyên hàm IIFE này vứt sang bên giao diện [GET] Login rồi chạy là OK
 // Hoặc đứng từ thư mục gốc chạy lệnh : node hassPassword.util.js
-module.exports = (async () => {
+
+(async () => {
   try {
-    const plainPassword = 'levana'; // thay bằng mật khẩu tại đây
+    const plainPassword = 'myPassword'; // thay cái mật khẩu bạn muốn tại đây
     const saltRounds = 10;
 
     const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
-    console.log('Hashed password:', hashedPassword);
+    console.log(hashedPassword);
   } catch (error) {
     console.error('Error hashing password:', error);
+    process.exit(1);
   }
 })();
